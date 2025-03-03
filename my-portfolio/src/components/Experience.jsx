e2eb4892ee9167d2d7ab896c55eb90d1cbe9eb20
xuanor <joancomic.jm@gmail.com>
2025-03-03 15:59:52 +0100

    add mobile styles in Experience.jsx

--- my-portfolio/src/components/Experience.jsx
+++ my-portfolio/src/components/Experience.jsx
@@ -10,39 +10,44 @@ export default function Experience() {
       className="mt-[100px] bg-[var(--navy)] min-h-screen flex items-center justify-center px-4"
     >
       <div className="max-w-[650px] w-full text-center bg-[var(--navy)] p-6">
+        
         <h2
-          className={`text-left text-4xl font-bold text-[var(--green)] transition-opacity duration-700  delay-200 ${
+          className={`text-left text-3xl sm:text-4xl font-bold text-[var(--green)] transition-opacity duration-700 delay-200 ${
             isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
           }`}
         >
           _Experience
         </h2>
-        <div className="bg-[var(--navy)] p-6 rounded-lg text-left">
-          <div className="relative">
-              {/* Línea vertical */}
-            <div className={`absolute left-[-25px] top-0 h-full w-[1px] bg-[var(--dark-slate)] transition-opacity duration-700 delay-700 ${
-                isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}></div>
-            <h3
-              className={`mt-6 mb-3 text-2xl text-[var(--lightest-slate)] transition-opacity duration-700 delay-300 ${
-                isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
-              }`}
-            >
-              Web Developer - [Openers & Closers]
-            </h3>
-            <p
-              className={`mt-1 mb-4 text-sm text-[var(--light-slate)] transition-opacity duration-700 delay-450 ${
-                isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
-              }`}
-            >
-              2024 - Present
-            </p>
+
+        <div className="bg-[var(--navy)] p-4 sm:p-6 rounded-lg text-left relative">
+          
+          <div className={`absolute left-[-15px] sm:left-[-25px] top-0 h-full w-[1px] bg-[var(--dark-slate)] transition-opacity duration-700 delay-700 ${
+            isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
+          }`}></div>
+
+          <h3
+            className={`mt-6 mb-3 sm:mb-5 text-xl sm:text-2xl text-[var(--lightest-slate)] transition-opacity duration-700 delay-300 ${
+              isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
+            }`}
+          >
+            Web Developer - [Openers & Closers]
+          </h3>
+
+          <p
+            className={`mt-1 mb-4 text-sm text-[var(--light-slate)] transition-opacity duration-700 delay-450 ${
+              isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
+            }`}
+          >
+            2024 - Present
+          </p>
+
+          <div className="w-full sm:w-auto mx-auto">
             <ol
-              className={`ml-3 list-disc text-md text-[var(--slate)] grid gap-x-3 gap-y-3 p-0 pl-5 mt-5 overflow-hidden transition-opacity duration-700 delay-550 ${
+              className={`list-disc text-md text-[var(--slate)] grid gap-x-3 gap-y-3 p-0 pl-5 mt-5 overflow-hidden transition-opacity duration-700 delay-550 ${
                 isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
               }`}
             >
-              <li>
-                  Design and write unit and integration tests, ensuring optimal code coverage and reducing errors before deployment.</li>
+              <li>Design and write unit and integration tests, ensuring optimal code coverage and reducing errors before deployment.</li>
               <li>Implement secure authentication using OAuth 2.0 and JWT, strengthening the protection of users' sensitive data.</li>
               <li>Develop serverless solutions on AWS Lambda, optimizing infrastructure costs and improving system scalability.</li>
               <li>Write clear and structured technical documentation, facilitating the integration of other teams with our APIs and services.</li>
